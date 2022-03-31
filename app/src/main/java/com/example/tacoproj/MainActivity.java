deabfa3230a68de2b06696b6d8514ada313dd9a8
5CentBanana <nicholasdienstbier@gmail.com>
2022-03-31 10:34:10 -0500

    FixedBuild.Gradle

--- app/src/main/java/com/example/tacoproj/MainActivity.java
+++ app/src/main/java/com/example/tacoproj/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.tacoproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayout gridLayout = new GridLayout(this);
+
+        Point size = new Point();
+        this.getWindowManager().getDefaultDisplay().getSize(size);
+        int buttonWidth = size.x/2;
+
+        
+
+
     }
 
     //hello
