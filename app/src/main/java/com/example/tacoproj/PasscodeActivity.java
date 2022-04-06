6bab07f98ed9936c5aa7240ef9ca135c6e6f97e8
5CentBanana <nicholasdienstbier@gmail.com>
2022-04-06 09:17:09 -0500

    Working on a password to get to dev scene

--- app/src/main/java/com/example/tacoproj/PasscodeActivity.java
+++ app/src/main/java/com/example/tacoproj/PasscodeActivity.java
@@ -2,13 +2,33 @@ package com.example.tacoproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.hanks.passcodeview.PasscodeView;
+
 public class PasscodeActivity extends AppCompatActivity {
+    PasscodeView passcodeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passcode);
+        passcodeView= findViewById(R.id.passcodeView);
+        passcodeView.setPasscodeLength(4)
+                .setLocalPasscode("1234")
+                .setListener(new PasscodeView.PasscodeViewListener() {
+                    @Override
+                    public void onFail() {
+
+                    }
+
+                    @Override
+                    public void onSuccess(String number) {
+
+                        startActivity(new Intent(PasscodeActivity.this,MainActivity.class));
+                        finish();
+                    }
+                });
     }
 }
