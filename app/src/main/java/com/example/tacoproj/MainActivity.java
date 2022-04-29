ca1fffc5e6fdbb64f0b837a20c68d3d82907a36c
Jadonsw123 <jadon.lee.swearingen@gmail.com>
2022-04-29 09:31:36 -0500

    added back cart activity i don't know why but elenas thing got rid of it I think

--- app/src/main/java/com/example/tacoproj/MainActivity.java
+++ app/src/main/java/com/example/tacoproj/MainActivity.java
@@ -110,6 +110,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void ClickProceed(View view) {
         Log.w("Deez", "Proceed to cart pressed");
+        Intent cartIntent = new Intent(this, CartActivity.class);
+        order.add("Taco egg");
+        order.add("Topping cheese");
+        order.add("Topping peppers");
+        cartIntent.putExtra("order",order);
+        this.startActivity(cartIntent);
     }
 
     public void ClickDevMenu(View view) {
